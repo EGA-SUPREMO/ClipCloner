e48f80856a45ac63986aa6a5e0e4f62842cb29a2
EGA-SUPREMO <cxiesestro@gmail.com>
2022-04-23 16:52:14 -0400

    oeu

--- main.py
+++ main.py
@@ -11,23 +11,25 @@ class Actor(object):
     """docstring for Actor"""
     def __init__(self, name, link, arroba):
         super(Actor, self).__init__()
-        self.name = name
+        self.name = name.split()
         self.link = link
         self.arroba = arroba
         Actor.currentId +=1
         self.id = Actor.currentId
-        print(self)
+        
 
 hey1=Actor("Shirakami Fubuki", "https://youtube.com/chennel/ceohurc", "Fubu Ch.")
-hey2=Actor("he","hey","hey")
+hey2=Actor("Sakura Miko","hey","Miko Ch.")
+members=[Actor("Shirakami Fubuki", "https://youtube.com/chennel/ceohurc", "Fubu Ch."), Actor("Sakura Miko","hey","Miko Ch.")]
 print(hey1.id)
 print(hey2.id)
 def getNames(title):
     words = title.split()
     for word in words:
-        if word in hey1.name:
-            print("yes")
-            print(word)
+        for member in members:
+            if word.lower() in member.name:
+                print("yes")
+                print(word)
 
 
-getNames("Fubuki se cae xd y mata a towa")
+getNames("Fubuki se cae xd y mata a Miko")
