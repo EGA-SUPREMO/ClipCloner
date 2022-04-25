c9a507695e31c98a888c65d127fa62ef46de6dac
EGA-SUPREMO <cxiesestro@gmail.com>
2022-04-25 08:12:50 -0400

    tegs

--- main.py
+++ main.py
@@ -3,28 +3,41 @@ import getmembers
 
 
 
-descrClip = "- Clip original:"
+descrClip = "- Clip original: "
 descrStream = "- Stream original: "
 descrChannel = "- "
-tags={"#Hololive", "#Vtuber"}
+tags=["#hololive", "#vtuber"]
 
+fullDescr = ""
 
 getmembers.getNames(sys.argv[1:][0])
 
+def setDescrClip():
+    global descrClip
+    global fullDescr
+    descrClip += sys.argv[1:][1]
+    fullDescr += descrClip
+
+setDescrClip()
+
 def setChannels():
     fulldescrChannel = ""
+    global fullDescr
 
     for i in range(len(getmembers.membersInClip)):
         fullName = getmembers.members[i].name[0] + " " + getmembers.members[i].name[1]
         fulldescrChannel += "\n" + descrChannel + fullName + ": " + getmembers.members[i].link
-    print(fulldescrChannel)
+    fullDescr +=  fulldescrChannel
 
 setChannels()
 
-def setDescrClip():
-    global descrClip
-    descrClip += sys.argv[1:][1]
+def setTags():
+    global tags, fullDescr
+    for i in range(len(getmembers.membersInClip)):
+        tags.insert(i + 1, "#" + getmembers.members[i].name[1].lower())
+    fullDescr += "\n"
+    for tag in tags:
+        fullDescr += tag + " "
+setTags()
 
-setDescrClip()
-
-print(descrClip)
+print(fullDescr)
--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 gettitle() {
-    TITLE=$(youtube-dl --skip-download --get-title --no-warnings $1)
+    TITLE=$(youtube-dl --skip-download --get-title --no-warnings --youtube-skip-dash-manifest $1)
     #echo "youtube-dl --skip-download --get-title --no-warnings $1"
-    python3 main.py $TITLE
+    youtube-dl --skip-download --no-warnings --write-description --youtube-skip-dash-manifest $1
+    python3 main.py $TITLE $1
 }
 
 gettitle "link de youtubu uwu"
