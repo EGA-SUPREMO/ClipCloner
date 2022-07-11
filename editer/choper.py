aa66574bb41f5cf9a78d6f16e45aa8b3646b0421
Mondestro <cxiesestro@gmail.com>
2022-07-11 09:33:53 -0400

    remove command in comments from choper

--- editer/choper.py
+++ editer/choper.py
@@ -1,9 +1,5 @@
 import os
 import sys
 
-os.system("ffmpeg -i clip.mp4 -c copy -map 0 -segment_time 00:00:01 -f segment -reset_timestamps 1 output%03d.mp4")
-
-
-# ffmpeg -i clip.mp4 -c:v libx265 -preset fast -crf 17 -ac 2 -vbr 3 -map 0 -segment_time 00:00:01 -f segment -async 1 -strict -2 zzzoutput%03d.mp4
-
-# ffmpeg -i clip.mp4 -map 0 -segment_time 00:00:01 -f segment -async 1 -strict -2 zzzoutput%03d.mp4
+def removeVideo():
+	os.system("ffmpeg -i clip.mp4 -vn clip_audio.mp4")
--- editer/shit.py
+++ editer/shit.py
@@ -15,10 +15,14 @@ def get_alignment_info(fps: list):
     return list(zip(file_specs, result))
 
 print(time.asctime())
-print(get_alignment_info(["parts/fixed_clip_audio010.mp4", "stream.mkv"]))
+info = get_alignment_info(["parts/fixed_clip_audio010.mp4", "stream.mkv"])
+print(info)
 print(time.asctime()) 
 
-
+print(info[0][1]['trim'])
+print(info[0][1]['pad'])
+print(info[0][1]['trim_post'])
+print(info[0][1]['pad_post'])
 
 
 #remove video
@@ -30,4 +34,4 @@ print(time.asctime())
 #    ffmpeg -y -ss 00:00:00 -i $i fixed_$i
 #done
 # [('/media/trabajo/1E8E46418E461227/Users/Tecnology Valencia/Downloads/uwu/Novaj git-oj/clip-generator/editer/parts/fixed_clip_audio010.mp4', {'trim': -0.0, 'pad': 20.053333333333335, 'orig_duration': 1.0, 'trim_post': 0.0, 'pad_post': 98.96666666666667, 'orig_streams': [{'type': 'Audio', 'sample_rate': 44100}], 'orig_streams_summary': {'max_resol_width': 0, 'max_resol_height': 0, 'max_sample_rate': 44100, 'max_fps': 0.0, 'num_video_streams': 0, 'num_audio_streams': 1}}), ('/media/trabajo/1E8E46418E461227/Users/Tecnology Valencia/Downloads/uwu/Novaj git-oj/clip-generator/editer/stream.mkv', {'trim': 20.053333333333335, 'pad': 0.0, 'orig_duration': 120.02, 'trim_post': 98.96666666666667, 'pad_post': 0.0, 'orig_streams': [{'type': 'Video', 'resolution': [[1920, 1080], '[SAR 1:1 DAR 16:9]'], 'fps': 60.0}, {'type': 'Audio', 'sample_rate': 44100}], 'orig_streams_summary': {'max_resol_width': 1920, 'max_resol_height': 1080, 'max_sample_rate': 44100, 'max_fps': 60.0, 'num_video_streams': 1, 'num_audio_streams': 1}})]
-Sun Jul 10 15:28:49 2022
+
