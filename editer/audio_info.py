74ce53c9fb70efe2b0f37811dd6e0f094c34e837
Mondestro <cxiesestro@gmail.com>
2022-07-11 10:27:11 -0400

    clean shit.py and rename into audio info

--- editer/audio_info.py
+++ editer/audio_info.py
@@ -2,8 +2,8 @@ from align_videos_by_soundtrack.align import SyncDetector
 from align_videos_by_soundtrack.align_params import *
 from align_videos_by_soundtrack.utils import *
 import time
-#    res = sd.align("hey.webm", "oo.webm")
 
+info = []
 
 def get_alignment_info(fps: list):
     file_specs = check_and_decode_filenames(fps, min_num_files=2)
@@ -14,10 +14,9 @@ def get_alignment_info(fps: list):
         result = det.align(file_specs, known_delay_map={})
     return list(zip(file_specs, result))
 
-print(time.asctime())
-info = get_alignment_info(["parts/fixed_clip_audio010.mp4", "stream.mkv"])
-print(info)
-print(time.asctime()) 
+def get_audio_infos():
+    info.extend(get_alignment_info(["parts/fixed_clip_audio010.mp4", "stream.mkv"]))
+
 
 print(info[0][1]['trim'])
 print(info[0][1]['pad'])
@@ -25,13 +24,5 @@ print(info[0][1]['trim_post'])
 print(info[0][1]['pad_post'])
 
 
-#remove video
-#ffmpeg -i clip.mp4 -vn clip_audio.mp4
-#cut by one second
-#ffmpeg -i clip_audio.mp4  -segment_time 00:00:01 -f segment -strict -2  -map 0 -c:a aac clip_audio%03d.mp4
-#fix cuts
-#for i in *.mp4 ; do
-#    ffmpeg -y -ss 00:00:00 -i $i fixed_$i
-#done
 # [('/media/trabajo/1E8E46418E461227/Users/Tecnology Valencia/Downloads/uwu/Novaj git-oj/clip-generator/editer/parts/fixed_clip_audio010.mp4', {'trim': -0.0, 'pad': 20.053333333333335, 'orig_duration': 1.0, 'trim_post': 0.0, 'pad_post': 98.96666666666667, 'orig_streams': [{'type': 'Audio', 'sample_rate': 44100}], 'orig_streams_summary': {'max_resol_width': 0, 'max_resol_height': 0, 'max_sample_rate': 44100, 'max_fps': 0.0, 'num_video_streams': 0, 'num_audio_streams': 1}}), ('/media/trabajo/1E8E46418E461227/Users/Tecnology Valencia/Downloads/uwu/Novaj git-oj/clip-generator/editer/stream.mkv', {'trim': 20.053333333333335, 'pad': 0.0, 'orig_duration': 120.02, 'trim_post': 98.96666666666667, 'pad_post': 0.0, 'orig_streams': [{'type': 'Video', 'resolution': [[1920, 1080], '[SAR 1:1 DAR 16:9]'], 'fps': 60.0}, {'type': 'Audio', 'sample_rate': 44100}], 'orig_streams_summary': {'max_resol_width': 1920, 'max_resol_height': 1080, 'max_sample_rate': 44100, 'max_fps': 60.0, 'num_video_streams': 1, 'num_audio_streams': 1}})]
 
