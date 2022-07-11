d66335f375a82d75a7fd2c4846267c1b4fa9ef05
Mondestro <cxiesestro@gmail.com>
2022-07-11 10:00:16 -0400

    chopper now chops good audio parts

--- editer/choper.py
+++ editer/choper.py
@@ -1,5 +1,24 @@
 import os
 import sys
 
+dirAudioParts = "audio_parts/"
+dirFixedAudioParts = "fixed_audio_parts/"
+
 def removeVideo():
-	os.system("ffmpeg -i clip.mp4 -vn clip_audio.mp4")
+	os.system("ffmpeg -y -i clip.mp4 -vn clip_audio.mp4")
+
+def cutAudioIntoOneSecondParts():
+	os.system("ffmpeg -y -i clip_audio.mp4  -segment_time 00:00:01 -f segment -strict -2  -map 0 -c:a aac "+ dirAudioParts +"clip_audio%03d.mp4")
+
+
+def fixAudioParts():
+	filenames = next(os.walk(dirAudioParts), (None, None, []))[2]
+	for filename in filenames:
+		os.system("ffmpeg -y -ss 00:00:00 -i " + dirAudioParts + filename + " " + dirFixedAudioParts + filename)
+
+
+
+
+#removeVideo()
+#cutAudioIntoOneSecondParts()
+#fixAudioParts()
