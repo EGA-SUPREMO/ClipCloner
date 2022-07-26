8ca08c7a5dab3c779270700ce4bc43620f8c4916
Mondestro <cxiesestro@gmail.com>
2022-07-26 14:31:13 -0400

    now every downloaded clip will be .mkv

--- main.py
+++ main.py
@@ -3,13 +3,4 @@ import sys
 import subprocess
 import descript.maini
 
-#clip_path = os.path.join('..', 'clip')
-#clip_path = os.path.join(os.getcwd(), script_path)
-#print(clip_path)
-
-os.system("youtube-dl -f \"bestvideo[height<=720]+bestaudio/best[height<=720]\" -o ../Clips/clip " + sys.argv[1:][0])
-
-#script_path = os.path.join('Description-generator', 'main.py')
-#script_path = os.path.join(os.getcwd(), script_path)
-
-#subprocess.Popen("python '"+script_path +"' '" + sys.argv[1:][0]+"'")
+os.system("youtube-dl -f \"bestvideo[height<=720]+bestaudio[ext=m4a]/bestvideo+bestaudio\" --merge-output-format mkv -o ../Clips/clip " + sys.argv[1:][0])
