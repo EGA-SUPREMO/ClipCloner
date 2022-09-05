cc0b36061a905793209712c56ef3346a5d627c71
EGA-SUPREMO <cxiesestro@gmail.com>
2022-09-05 14:53:37 -0400

    fix chopper tests

--- clip_generator/editter/chopper.py
+++ clip_generator/editter/chopper.py
@@ -1,31 +1,26 @@
 import os
 import sys
 
-from clip_generator.editter.dirs import dir_clip
-from clip_generator.editter.dirs import dir_audio_clip
-from clip_generator.editter.dirs import dirAudioParts
-from clip_generator.editter.dirs import dirFixedAudioParts
-from clip_generator.editter.dirs import dir_stream
-from clip_generator.editter.dirs import dir_trimmed_stream
+import clip_generator.editter.dirs as dirs
 
 def removeVideo():
-	os.system(f"ffmpeg -loglevel error -stats -y -i {dir_clip} -vn {dir_audio_clip}")
+	os.system(f"ffmpeg -loglevel error -stats -y -i {dirs.dir_clip} -vn {dirs.dir_audio_clip}")
 
 # Input: String: seconds in %01d
 def cutAudioIntoXSecondsParts(x):
-	os.system(f"ffmpeg -loglevel error -stats -y -i {dir_audio_clip}  -segment_time 00:00:{x} -f segment -strict -2  -map 0 -c:a aac {dirAudioParts}S{x}_clip_audio%01d.mp4")
+	os.system(f"ffmpeg -loglevel error -stats -y -i {dirs.dir_audio_clip}  -segment_time 00:00:{x} -f segment -strict -2  -map 0 -c:a aac {dirs.dirAudioParts}S{x}_clip_audio%01d.mp4")
 
 # Input: Int: length of cutted audio from the last seconds
 def cutLastSecondsAudio(seconds):
 	cutted_seconds = str(seconds+1)
 	real_seconds = str(seconds)
-	os.system(f"ffmpeg -loglevel error -stats -y -sseof -{cutted_seconds} -i {dir_audio_clip} -c copy {dirAudioParts}temp_last_S{real_seconds}_clip_audio.mp4")
-	os.system("ffmpeg -loglevel error -stats -y -ss 0 -to 00:00:03 -i "+ dirAudioParts +"temp_last_S"+ real_seconds +"_clip_audio.mp4 -c copy "+ dirAudioParts +"last_S"+ real_seconds +"_clip_audio.mp4")
+	os.system(f"ffmpeg -loglevel error -stats -y -sseof -{cutted_seconds} -i {dirs.dir_audio_clip} -c copy {dirs.dirAudioParts}temp_last_S{real_seconds}_clip_audio.mp4")
+	os.system("ffmpeg -loglevel error -stats -y -ss 0 -to 00:00:03 -i "+ dirs.dirAudioParts +"temp_last_S"+ real_seconds +"_clip_audio.mp4 -c copy "+ dirs.dirAudioParts +"last_S"+ real_seconds +"_clip_audio.mp4")
 
 def fixAudioParts():
-	filenames = next(os.walk(dirAudioParts), (None, None, []))[2]
+	filenames = next(os.walk(dirs.dirAudioParts), (None, None, []))[2]
 	for filename in filenames:
-		os.system("ffmpeg -loglevel error -stats -y -ss 00:00:00 -i " + dirAudioParts + filename + " " + dirFixedAudioParts + filename)
+		os.system("ffmpeg -loglevel error -stats -y -ss 00:00:00 -i " + dirs.dirAudioParts + filename + " " + dirs.dirFixedAudioParts + filename)
 
 def chop(from_second, to_second):
-	os.system(f"ffmpeg -loglevel error -stats -y -ss {from_second} -to {to_second} -i {dir_stream} {dir_trimmed_stream}")
+	os.system(f"ffmpeg -loglevel error -stats -y -ss {from_second} -to {to_second} -i {dirs.dir_stream} {dirs.dir_trimmed_stream}")
