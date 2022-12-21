cead38874048b211d3b5831ab593527cef51562a
EGA-SUPREMO <cxiesestro@gmail.com>
2022-12-21 17:17:49 -0400

    add temp debugging info in offset and kinda broken

--- clip_generator/editter/compare_sound_by_images/offset.py
+++ clip_generator/editter/compare_sound_by_images/offset.py
@@ -1,8 +1,10 @@
 from PIL import Image
 
+from matplotlib import pyplot as plt
+
 # Open the two images
-image1 = Image.open('clip1.png')
-image2 = Image.open('stream1.png')
+clip_image = Image.open('clip30.png')
+stream_image = Image.open('stream.png')
 
 def blending(image1, image2, offset_x):
     # Add an offset to image2 by pasting it onto a blank image and then resizing it
@@ -11,9 +13,10 @@ def blending(image1, image2, offset_x):
     offset_image = offset_image.resize(image2.size)
 
     # Get the size of the intersection of the two images
-    intersection_width = min(image1.size[0], image2.size[0] - offset_x)
+    intersection_width = min(image1.size[0] - offset_x, image2.size[0])
     intersection_height = min(image1.size[1], image2.size[1])
 
+    offset_image = offset_image.resize(image1.size)
     # Blend the images
     blended_image = Image.blend(image1, offset_image, alpha=0.5)
 
@@ -25,12 +28,16 @@ def blending(image1, image2, offset_x):
 
     return cropped_image
 
-for x in range(image1.width):
-    blended_image = blending(image1, image2, x)
+
+def relation_percentage(value1: int, value2: int) -> float:
+    value1 = max(value1, 1)
+    value2 = max(value2, 1)
+    
+    return 100 * value1 / (value1 + value2)
 
 # Iterate over the pixels and count the red ones
 def count_colored_pixels(pixels: list[tuple[int, int, int, int]]) -> dict[str, int]:
-    result = {'purple': 0, 'blue': 0, 'red': 0, 'other': 0}
+    result = {'purple': 0, 'blue': 0, 'red': 0, 'none': 0, 'other': 0}
     for pixel in pixels:
         match(pixel):
             case (127, 0, 127, 254):
@@ -40,14 +47,32 @@ def count_colored_pixels(pixels: list[tuple[int, int, int, int]]) -> dict[str, i
             case (127, 0, 0, 127):
                 result['red'] += 1
             case (0, 0, 0, 0):
-                pass
+                result['none'] += 1
             case _:
                 result['other'] += 1
     return result
 
-# Get the pixel data for the image
-pixels = blended_image.getdata()
-result = count_colored_pixels(pixels)
+line = []
+lineamount = []
+line_average = []
+for x in range(stream_image.width):
+    blended_image = blending(clip_image, stream_image, x)
+    # Get the pixel data for the image
+    pixels = blended_image.getdata()
+    result = count_colored_pixels(pixels)
+    mismatch_pixels = result["red"] + result["blue"] + result["other"]
+    accuracy = relation_percentage(result["purple"], mismatch_pixels)
+    amount = relation_percentage(result["purple"] + mismatch_pixels, result["none"] + (x * clip_image.height))
+
+    line.append(accuracy)
+    lineamount.append(amount)
+    line_average.append((accuracy * 0.3) + (amount * 0.7))
+
+fig, ax = plt.subplots()
+ax.plot(line, color="red")
+ax.plot(lineamount, color="blue")
+ax.plot(line_average, color="green")
+fig.savefig("cross-correlation10.png")
 
 print(f'Number of red pixels: {result["red"]}')
 print(f'Number of blue pixels: {result["blue"]}')
