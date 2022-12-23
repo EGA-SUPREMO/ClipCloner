0f4c157a0d73b700b8585de9b969d4b7ea3922e5
EGA-SUPREMO <cxiesestro@gmail.com>
2022-12-23 14:23:33 -0400

    add test for offset

---
+++ tests/testOffset.py
@@ -0,0 +1,31 @@
+import unittest
+
+from clip_generator.editter.compare_sound_by_image import relation_percentage
+
+class TestRelationPercentage(unittest.TestCase):
+    def test_value1_greater_than_value2(self):
+        value1 = 10
+        value2 = 5
+        expected_result = 66.66666666666666
+        self.assertAlmostEqual(relation_percentage(value1, value2), expected_result)
+
+    def test_value2_greater_than_value1(self):
+        value1 = 5
+        value2 = 10
+        expected_result = 33.333333333333336
+        self.assertAlmostEqual(relation_percentage(value1, value2), expected_result)
+
+    def test_value1_equal_to_0(self):
+        value1 = 0
+        value2 = 5
+        expected_result = 0.0
+        self.assertAlmostEqual(relation_percentage(value1, value2), expected_result)
+
+    def test_value2_equal_to_0(self):
+        value1 = 5
+        value2 = 0
+        expected_result = 100.0
+        self.assertAlmostEqual(relation_percentage(value1, value2), expected_result)
+
+if __name__ == '__main__':
+    unittest.main
