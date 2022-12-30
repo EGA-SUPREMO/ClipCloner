a85c4252f0432f06561d5fb80e4211c3611a8e61
EGA-SUPREMO <cxiesestro@gmail.com>
2022-12-30 09:35:41 -0400

    add perfomance test for offset, not finished

---
+++ tests/perfomance_tests/offsetByImagePerfomance.py
@@ -0,0 +1,30 @@
+import timeit
+
+from PIL import Image
+
+from clip_generator.editter.compare_sound_by_images.offset import compare_images
+
+# Test scenario 1: compare a normal worstaudio stream of 13K seconds, nene gets tricked by viewers
+clip_image = Image.open('clip2022.png')
+stream_image = Image.open('stream2022.png')
+offsets = range(stream_image.width)
+
+return_values = compare_images(clip_image, stream_image, offsets)
+execution_time = timeit.timeit(lambda: return_values, number=1)
+print(f'Execution time for test scenario 1: {execution_time:.6f} seconds')
+
+print(f'Return values: {return_values}')
+
+# # Test scenario 2: compare two large images with a large offset
+# clip_image = # large image data
+# stream_image = # large image data
+# offsets = (1000, 1000)
+# execution_time = timeit.timeit(lambda: compare_images(clip_image, stream_image, offsets), number=1)
+# print(f'Execution time for test scenario 2: {execution_time:.6f} seconds')
+
+# # Test scenario 3: compare two images with different sizes and a small offset
+# clip_image = # small image data
+# stream_image = # large image data
+# offsets = (10, 10)
+# execution_time = timeit.timeit(lambda: compare_images(clip_image, stream_image, offsets), number=1)
+# print(f'Execution time for test scenario 3: {execution_time:.6f} seconds')
