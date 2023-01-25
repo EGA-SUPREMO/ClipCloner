1e991722ccd8df5cd110de429abdd13809198bca
EGA-SUPREMO <cxiesestro@gmail.com>
2023-01-25 11:09:27 -0400

    add ability to change phase in justTrim

--- clip_generator/justTrim.py
+++ clip_generator/justTrim.py
@@ -3,9 +3,13 @@ import sys
 
 credits_offset=0
 is_video=0
+phase=0
 
 if __name__ == '__main__':
 	is_video = sys.argv[1:][0].lower() == "true"
 	credits_offset = int(sys.argv[1:][1])
+	
+	if len(sys.argv[1:])>2:
+		phase = int(sys.argv[1:][2])
 
-	trimmer.trim_to_clip(is_video, offset_credits=credits_offset)
+	trimmer.trim_to_clip(is_video, offset_credits=credits_offset, phase=phase)
