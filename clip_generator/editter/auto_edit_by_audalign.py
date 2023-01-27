51c915a336992732ed7e86ad9e9c3a3a9e24047c
EGA-SUPREMO <cxiesestro@gmail.com>
2023-01-27 12:19:11 -0400

    remove comments in auto_edit by audalign and add raw timestamps
    for tests

--- clip_generator/editter/auto_edit_by_audalign.py
+++ clip_generator/editter/auto_edit_by_audalign.py
@@ -1,17 +1,15 @@
+import os
+
 import audalign as ad
 
 from clip_generator.editter import dirs
 
 
 def get_offset(filename: str):
-    # fingerprint_rec = ad.FingerprintRecognizer()
+    #fingerprint_rec = ad.FingerprintRecognizer()
     fingerprint_rec = ad.CorrelationRecognizer()
 
-    # fingerprint_rec.config.set_accuracy(3)
-    # results = ad.align("../test_borrar/", recognizer=fingerprint_rec)
-
-    # ad.convert_audio_file("../clip.mkv", "../test_borrar/clip.wav")
-    ad.convert_audio_file("../stream.mkv", "../test_borrar/stream.wav")
+    #fingerprint_rec.config.set_accuracy(3)
 
     results = ad.align_files(dirs.dirFixedAudioParts + filename, dirs.dir_audio_stream, recognizer=fingerprint_rec)
 
