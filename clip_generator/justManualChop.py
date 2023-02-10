b5c1113c4e8305c865435a3c77ef64c206d381bb
EGA-SUPREMO <cxiesestro@gmail.com>
2023-02-10 08:04:00 -0400

    add manual chop, where I can just chop with a manual timestamp

---
+++ clip_generator/justManualChop.py
@@ -0,0 +1,15 @@
+import clip_generator.editter.chopper as chopper
+import clip_generator.editter.dirs as dirs
+import sys
+import ast
+
+time_intervals = []
+
+if __name__ == '__main__':
+    try:
+        time_intervals = ast.literal_eval(sys.argv[1:][0])
+        print(time_intervals)
+    except IndexError:
+        pass
+
+    chopper.final_chop(dirs.dir_stream, dirs.dir_edited_stream, time_intervals)
