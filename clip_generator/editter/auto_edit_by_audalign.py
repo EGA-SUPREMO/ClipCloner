8db6f4e595e89ca8810779ee296afb314ee73d61
EGA-SUPREMO <cxiesestro@gmail.com>
2023-02-10 08:04:33 -0400

    comments, irrelevant

--- clip_generator/editter/auto_edit_by_audalign.py
+++ clip_generator/editter/auto_edit_by_audalign.py
@@ -7,12 +7,13 @@ from clip_generator.editter import dirs
 
 def get_offset(filename: str):
     #fingerprint_rec = ad.FingerprintRecognizer()
-    fingerprint_rec = ad.CorrelationRecognizer()
-
+    corr_rec = ad.CorrelationRecognizer()
+    #corr_rec = ad.VisualRecognizer()
+    #corr_rec.config.normalize = True
+    corr_rec.config.sample_rate = 4000
     #fingerprint_rec.config.set_accuracy(3)
 
-    results = ad.align_files(dirs.dirFixedAudioParts + filename, dirs.dir_audio_stream, recognizer=fingerprint_rec)
+    results = ad.align_files(dirs.dirFixedAudioParts + filename, dirs.dir_audio_stream, recognizer=corr_rec)
 
     return results[filename]
-    # ad.pretty_print_alignment(results)
     # fingerprint_rec.save_fingerprinted_files("../test_borrar/save_file.json")
