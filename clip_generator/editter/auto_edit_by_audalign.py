f5dd51038e43129e4c85db71cd4fec723cfc6434
EGA-SUPREMO <cxiesestro@gmail.com>
2023-03-13 15:55:06 -0400

    remove unused code and add get offset for trim

--- clip_generator/editter/auto_edit_by_audalign.py
+++ clip_generator/editter/auto_edit_by_audalign.py
@@ -12,6 +12,7 @@ def get_offset(filename: str):
     #corr_rec = ad.CorrelationSpectrogramRecognizer()
     corr_rec.config.normalize = True
     corr_rec.config.sample_rate = 8000
+    corr_rec.config.match_len_filter = 100
     #corr_rec.config.set_accuracy(1)
 #    corr_rec.save_fingerprinted_files("../test_borrar/save_file.json")
 #    corr_rec.load_fingerprinted_files("../test_borrar/save_file.json")
@@ -19,7 +20,23 @@ def get_offset(filename: str):
     results = ad.align_files(dirs.dirFixedAudioParts + filename, dirs.dir_audio_stream, recognizer=corr_rec)
     #corr_rec.save_fingerprinted_files("../test_borrar/save_file.json")
 
+    print(results['match_info'][filename]['match_info']['stream_audio.mp4']['offset_seconds'])
     try:
         return results[filename]
+    except Exception:
+        return 9999999
+
+
+# TODO needs tests
+def get_offset_for_trim():
+    corr_rec = ad.CorrelationRecognizer()
+    corr_rec.config.normalize = True
+    corr_rec.config.sample_rate = 8000
+    corr_rec.config.match_len_filter = 1000
+
+    results = ad.align_files(dirs.dir_audio_clip, dirs.dir_audio_stream, recognizer=corr_rec)
+
+    try:
+        return results['match_info'][os.path.basename(dirs.dir_audio_clip)]['match_info'][os.path.basename(dirs.dir_audio_stream)]['offset_seconds']
     except Exception:
         return 9999999
